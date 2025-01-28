ef64fb7825dae0db3a3db441adcbd1570a88e468
ZShab Niba <44338441+nibazshab@users.noreply.github.com>
2025-01-28 15:40:44 +0800

    key -> secret

--- aria2/tracker.sh
+++ aria2/tracker.sh
@@ -1,10 +1,7 @@
 #!/bin/sh
-
+secret=password
 tracker=$(curl -sL https://trackerslist.com/all_aria2.txt)
-
-key=password
-
 [ -n $tracker ] && {
-  curl -d '{"id":"1","method":"aria2.changeGlobalOption","params":["token:'$key'",{"bt-tracker":"'$tracker'"}]}' http://localhost:6800/jsonrpc
+  curl -d '{"id":"1","method":"aria2.changeGlobalOption","params":["token:'$secret'",{"bt-tracker":"'$tracker'"}]}' http://localhost:6800/jsonrpc
   sed -i "s@^\(bt-tracker=\).*@\1$tracker@" /home/data/aria2/aria2.conf
 }
