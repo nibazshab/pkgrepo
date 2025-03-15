ec6cc59233ed637d0b30ca0e407175dd8f3f52ee
nibazshab <44338441+nibazshab@users.noreply.github.com>
2025-03-15 10:48:02 +0000

    自动获取 secret

--- aria2/tracker.sh
+++ aria2/tracker.sh
@@ -1,7 +1,12 @@
 #!/bin/sh
-secret=password
+
+file="/home/data/aria2/aria2.conf"
+url=http://localhost:6800/jsonrpc
+
 tracker=$(curl -sL https://trackerslist.com/all_aria2.txt)
+secret="$(awk -F= '/^rpc-secret/ {print $2}' $file)"
+
 [ -n $tracker ] && {
-  curl -d '{"id":"1","method":"aria2.changeGlobalOption","params":["token:'$secret'",{"bt-tracker":"'$tracker'"}]}' http://localhost:6800/jsonrpc
-  sed -i "s@^\(bt-tracker=\).*@\1$tracker@" /home/data/aria2/aria2.conf
+  curl -d '{"id":"1","method":"aria2.changeGlobalOption","params":["token:'$secret'",{"bt-tracker":"'$tracker'"}]}' $url
+  sed -i "s@^\(bt-tracker=\).*@\1$tracker@" $file
 }
